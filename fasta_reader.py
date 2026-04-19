675ae4965dfc974d01df30d480bd329aaddfa2f5
hannahmohdrawi <hannahmohdrawi@hotmail.com>
2026-04-19 21:03:06 +0100

    Adding hydrophobicity calc

--- fasta_reader.py
+++ fasta_reader.py
@@ -95,6 +95,19 @@ def calculate_pI(counts):
 
     return best_pH
 
+# Kyte-Doolittle hydrophobicity scale
+hydrophobicity_scale = {
+    'A': 1.8, 'R': -4.5, 'N': -3.5, 'D': -3.5,
+    'C': 2.5, 'E': -3.5, 'Q': -3.5, 'G': -0.4,
+    'H': -3.2, 'I': 4.5, 'L': 3.8, 'K': -3.9,
+    'M': 1.9, 'F': 2.8, 'P': -1.6, 'S': -0.8,
+    'T': -0.7, 'W': -0.9, 'Y': -1.3, 'V': 4.2
+}
+
+def average_hydrophobicity(seq):
+    values = [hydrophobicity_scale.get(aa, 0) for aa in seq]
+    return sum(values) / len(values)
+
 
 def aa_composition(seq):
     counts = Counter(seq)
@@ -113,6 +126,9 @@ def extract_features(seq):
     # pI
     counts = get_counts(seq)
     features["pI"] = calculate_pI(counts)
+
+    # Hydrophobicity 
+    features["hydrophobicity"] = average_hydrophobicity(seq)
 
     # Amino acid composition (20 features)
     comp = aa_composition(seq)
